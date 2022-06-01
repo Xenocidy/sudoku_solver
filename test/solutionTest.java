db42525c025e71cdf20def7fdb39e7991259593d
Chak Wong <jackywong3883@gmail.com>
2022-06-01 16:55:55 -0400

    Setup testing

---
+++ test/solutionTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class solutionTest {
+    
+    @Test 
+    public void test() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
