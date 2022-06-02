921f55b3772c335b8b1bd95099c14c94f84d19eb
Chak Wong <jackywong3883@gmail.com>
2022-06-02 15:17:37 -0400

    Improved algorithm traversal

--- src/main/solution.java
+++ src/main/solution.java
@@ -1,4 +1,5 @@
 package src.main;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,11 +8,23 @@ public class solution {
 
     /**
      * Takes in an unsolved sudoku board and return a solved board
+     * 
      * @param board Uncompleted sudoku board
      * @return board Completed sudoku board
      */
     public String[][] solveSudoku(String[][] board) {
-        
+        // public static String[][] solveSudoku() {
+        // String[][] board = {
+        // { ".", "1", "3", "7", ".", "4", "5", "8", "9" },
+        // { ".", "5", "4", ".", "9", ".", ".", ".", "." },
+        // { ".", "8", "2", "1", "5", "6", ".", "7", "3" },
+        // { ".", "7", ".", ".", "6", ".", ".", ".", "." },
+        // { "1", "9", ".", ".", ".", ".", ".", "4", "7" },
+        // { "3", ".", ".", ".", ".", "1", "2", "5", "." },
+        // { ".", ".", ".", "6", "3", ".", "7", "9", "." },
+        // { "5", ".", ".", ".", ".", ".", ".", ".", "2" },
+        // { ".", "3", ".", "2", ".", "7", ".", ".", "." } };
+
         Set<String> box1 = new HashSet<String>();
         box1.add("1");
         box1.add("2");
@@ -74,6 +87,9 @@ public class solution {
         // verify if board is valid
 
         // record all available values within each structure
+        // queues are used to record open positions
+        ArrayList<String> rQueue = new ArrayList<>();
+        ArrayList<String> cQueue = new ArrayList<>();
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
 
@@ -196,182 +212,191 @@ public class solution {
                         break;
                 }
 
+                // record the positions that are empty
+                if (!isNumeric(board[row][col])) {
+                    rQueue.add(Integer.toString(row));
+                    cQueue.add(Integer.toString(col));
+                }
+
             }
         }
 
         // iterate until board is complete
-        while (box1.size() != 0 || box2.size() != 0 || box3.size() != 0 || box4.size() != 0 ||
-                box5.size() != 0 || box6.size() != 0 || box7.size() != 0 || box8.size() != 0 || box9.size() != 0) {
+        while (!rQueue.isEmpty() || !cQueue.isEmpty()) {
 
-            // traverse thru the board
-            for (int row = 0; row < 9; row++) {
-                for (int col = 0; col < 9; col++) {
+            ArrayList<String> available = new ArrayList<>();
+            int row = Integer.valueOf(rQueue.remove(0));
+            int col = Integer.valueOf(cQueue.remove(0));
 
-                    // ignore filled values
-                    if (!isNumeric(board[row][col])) {
-                        ArrayList<String> available = new ArrayList<>();
+            // determine the subbox it is in
+            if (row <= 2 && col <= 2) {
 
-                        // determine the subbox it is in
-                        if (row <= 2 && col <= 2) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box1) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box1.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (row <= 2 && 3 <= col && col <= 5) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box2) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box2.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (row <= 2 && 6 <= col && col <= 8) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box3) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box3.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (3 <= row && row <= 5 && col <= 2) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box4) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box4.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (3 <= row && row <= 5 && 3 <= col && col <= 5) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box5) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box5.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (3 <= row && row <= 5 && 6 <= col && col <= 8) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box6) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box6.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (6 <= row && row <= 8 && col <= 2) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box7) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box7.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (6 <= row && row <= 8 && 3 <= col && col <= 5) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box8) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box8.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        } else if (6 <= row && row <= 8 && 6 <= col && col <= 8) {
-
-                            // record all overlapping values between each subbox, column, and row
-                            for (String value : box9) {
-                                if (rows.get(row).contains(value) && cols.get(col).contains(value))
-                                    available.add(value);
-                            }
-
-                            // if only one available answer possible, record it and remove it as available
-                            // within each structure
-                            if (available.size() == 1) {
-                                board[row][col] = available.get(0);
-                                box9.remove(available.get(0));
-                                rows.get(row).remove(available.get(0));
-                                cols.get(col).remove(available.get(0));
-                            }
-
-                        }
-
-                    }
-
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box1) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
                 }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box1.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (row <= 2 && 3 <= col && col <= 5) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box2) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box2.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (row <= 2 && 6 <= col && col <= 8) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box3) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box3.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (3 <= row && row <= 5 && col <= 2) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box4) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box4.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (3 <= row && row <= 5 && 3 <= col && col <= 5) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box5) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box5.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (3 <= row && row <= 5 && 6 <= col && col <= 8) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box6) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box6.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (6 <= row && row <= 8 && col <= 2) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box7) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box7.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (6 <= row && row <= 8 && 3 <= col && col <= 5) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box8) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box8.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
+            } else if (6 <= row && row <= 8 && 6 <= col && col <= 8) {
+
+                // record all overlapping values between each subbox, column, and row
+                for (String value : box9) {
+                    if (rows.get(row).contains(value) && cols.get(col).contains(value))
+                        available.add(value);
+                }
+
+                // if only one available answer possible, record it and remove it as available
+                // within each structure
+                if (available.size() == 1) {
+                    board[row][col] = available.get(0);
+                    box9.remove(available.get(0));
+                    rows.get(row).remove(available.get(0));
+                    cols.get(col).remove(available.get(0));
+                    continue;
+                }
+
             }
-        
+
+            // re-queue the position if cannot be solved right now
+            rQueue.add(String.valueOf(row));
+            cQueue.add(String.valueOf(col));
+
         }
 
         // System.out.println(box1);
@@ -405,10 +430,10 @@ public class solution {
         // System.out.println(col8);
 
         for (int row = 0; row < 9; row++) {
-        for (int col = 0; col < 9; col++) {
-        System.out.print(board[row][col] + " ");
-        }
-        System.out.println();
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
         }
 
         return board;
@@ -417,6 +442,7 @@ public class solution {
 
     /**
      * Check to see if a board value is a valid sudoku value
+     * 
      * @param str String of board value
      * @return boolean if str is a 0-9 value
      */
@@ -428,6 +454,6 @@ public class solution {
     }
 
     public static void main(String[] args) {
-        //solveSudoku();
+        // solveSudoku();
     }
 }
